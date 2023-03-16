5fde0861a6e69a9fe8e4e68d0e41d513f3e5b779
Anton Titov <anton.titov97@gmail.com>
2023-03-16 19:44:01 +0300

    body.java

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public int calculate(int weight, double hight) {
-        double bmi = weight/(hight*2);
+        double bmi = weight / (hight * 2);
         return (int) bmi;
     }
 }
